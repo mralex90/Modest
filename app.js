432bc33e411c6c07a870bc69ea66305f8b996b1a
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-10-11 23:27:00 +0300

    onclicl btn

---
+++ app.js
@@ -0,0 +1,12 @@
+function func1() {
+    z = z + 22;
+    console.log(z);
+}
+
+console.log('Hello');
+document.querySelector('h1').innerHTML = 'Hi';
+let z = 55;
+console.log(z);
+
+document.querySelector('.b-1').onclick = func1;
+
